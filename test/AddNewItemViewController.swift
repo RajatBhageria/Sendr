298ca39b62601b518bf6f78a3f7c1935f00a56eb
Johan K. Jensen <jj@johanjensen.dk>
2015-09-05 12:35:05 -0400

    Add tableview to add new request

---
+++ test/AddNewItemViewController.swift
@@ -0,0 +1,46 @@
+//
+//  AddNewItemViewController.swift
+//  test
+//
+//  Created by Johan K. Jensen on 05/09/2015.
+//  Copyright (c) 2015 adarsh bhatt. All rights reserved.
+//
+
+import UIKit
+
+class AddNewItemViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+//        cell.labelText.text = "Cell Text"
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
