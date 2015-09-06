30b09ddbe8868bea1bc3e908f5d7e660167ea8e9
Johan K. Jensen <jj@johanjensen.dk>
2015-09-05 20:15:16 -0400

    Add items to the feed

--- test/AddNewItemFormViewController.swift
+++ test/AddNewItemFormViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class AddNewItemFormViewController: XLFormViewController {
 
@@ -32,7 +33,7 @@ class AddNewItemFormViewController: XLFormViewController {
         let cancelBarButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.Plain, target: self, action: "cancel:")
         self.navigationItem.leftBarButtonItem = cancelBarButton
         
-        let doneBarButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Plain, target: self, action: "done:")
+        let doneBarButton = UIBarButtonItem(title: "Submit", style: UIBarButtonItemStyle.Plain, target: self, action: "done:")
         self.navigationItem.rightBarButtonItem = doneBarButton
         
         initializeForm()
@@ -44,8 +45,85 @@ class AddNewItemFormViewController: XLFormViewController {
     }
     
     func done(sender: AnyObject?) {
-        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        let validationErrors : Array<NSError> = self.formValidationErrors() as! Array<NSError>
+        if (validationErrors.count > 0){
+//            self.showFormValidationError(validationErrors.first)
+            validateForm()
+            return
+        }
+        self.tableView.endEditing(true)
+//        let alertView = UIAlertView(title: "Valid Form", message: "No errors found", delegate: self, cancelButtonTitle: "OK")
+//        alertView.show()
+        
+        let formValues = self.formValues() as! [String: AnyObject]
+        let address = (formValues["location"] as! NSString).stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+        let dataUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyB8w5z0QNIWZLokQwuMzVsiIgH-u6NRQJg"
+        let url = NSURL(string: dataUrl)!
+        
+        let downloadTask: NSURLSessionDataTask = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
+            
+            let res = NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers | .AllowFragments, error: nil) as! [String: AnyObject]
+            if let res2 = res["results"] as? [[String: AnyObject]] {
+                if let geometry = res2[0]["geometry"] as? [String: AnyObject] {
+                    if let location = geometry["location"] as? [String: AnyObject] {
+                        let lat = location["lat"]! as! Double
+                        let lng = location["lng"]! as! Double
+                        
+                        //title, created by, from datae, to date, location
+                        
+                        var parseObject = PFObject(className: "RentFeed")
+                        parseObject["location"] = PFGeoPoint(latitude: lat, longitude: lng)
+                        parseObject["createdBy"] = PFUser.currentUser()
+                        parseObject["title"] = formValues["title"]
+                        parseObject["notes"] = formValues["notes"]
+                        parseObject["toDate"] = formValues["toDate"]
+                        parseObject["toDate"] = formValues["fromDate"]
+                        parseObject.saveInBackgroundWithBlock({ (success, error) -> Void in
+                            if success {
+                                self.dismissViewControllerAnimated(true, completion: nil)
+                            } else {
+                                let alertView = UIAlertView(title: "Didn’t succeed posting", message: ":(", delegate: self, cancelButtonTitle: "OK")
+                                alertView.show()
+                            }
+                        })
+                        
+                        
+                    }
+                }
+            }
+        })
+        downloadTask.resume()
     }
+    
+    
+    func validateForm() {
+        let array = self.formValidationErrors()
+        for errorItem in array {
+            let error = errorItem as! NSError
+            let validationStatus : XLFormValidationStatus = error.userInfo![XLValidationStatusErrorKey] as! XLFormValidationStatus
+            if validationStatus.rowDescriptor!.tag == Tags.DateInline.rawValue ||
+                validationStatus.rowDescriptor!.tag == "title" ||
+                validationStatus.rowDescriptor!.tag == "notes" ||
+                validationStatus.rowDescriptor!.tag == "location" {
+                    if let cell = self.tableView.cellForRowAtIndexPath(self.form.indexPathOfFormRow(validationStatus.rowDescriptor)!) {
+                        self.animateCell(cell)
+                    }
+            }
+        }
+    }
+    
+    func animateCell(cell: UITableViewCell) {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values =  [0, 20, -20, 10, 0]
+        animation.keyTimes = [0, (1 / 6.0), (3 / 6.0), (5 / 6.0), 1]
+        animation.duration = 0.3
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        animation.additive = true
+        cell.layer.addAnimation(animation, forKey: "shake")
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -68,14 +146,22 @@ class AddNewItemFormViewController: XLFormViewController {
         row.required = true
         section.addFormRow(row)
         
+        // Location
+        row = XLFormRowDescriptor(tag: "location", rowType: XLFormRowDescriptorTypeText)
+        row.cellConfigAtConfigure["textField.placeholder"] = "Location"
+        row.required = true
+        section.addFormRow(row)
+        
         // Date From
-        row = XLFormRowDescriptor(tag: Tags.DateInline.rawValue, rowType: XLFormRowDescriptorTypeDateTimeInline, title:"From Date & Time")
+        row = XLFormRowDescriptor(tag: "toDate", rowType: XLFormRowDescriptorTypeDateTimeInline, title:"From Date & Time")
         row.value = NSDate()
+        row.required = true // Dates will always have a value (by default `now`)
         section.addFormRow(row)
         
         // Date To
-        row = XLFormRowDescriptor(tag: Tags.DateInline.rawValue, rowType: XLFormRowDescriptorTypeDateTimeInline, title:"To Date & Time")
-        row.value = NSDate()
+        row = XLFormRowDescriptor(tag: "fromDate", rowType: XLFormRowDescriptorTypeDateTimeInline, title:"To Date & Time")
+        row.value = NSDate(timeIntervalSinceNow: 60*60*24)
+        row.required = true
         section.addFormRow(row)
         
         // Notes
@@ -91,13 +177,13 @@ class AddNewItemFormViewController: XLFormViewController {
     
     override func formRowDescriptorValueHasChanged(formRow: XLFormRowDescriptor!, oldValue: AnyObject!, newValue: AnyObject!) {
         super.formRowDescriptorValueHasChanged(formRow, oldValue: oldValue, newValue: newValue)
-        if  formRow.tag ==  Tags.DatePicker.rawValue {
+        if formRow.tag ==  Tags.DatePicker.rawValue {
             let alertView = UIAlertView(title: "DatePicker", message: "Value Has changed!", delegate: self, cancelButtonTitle: "OK")
             alertView.show()
         }
     }
 
-    
+
 
     /*
     // MARK: - Navigation
